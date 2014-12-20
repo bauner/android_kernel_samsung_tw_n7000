651be8eb3f301d405ea1ad048c0628d1fa10fe0e
Christian Neumüller <cn00@gmx.at>
2014-12-20 23:59:48 +0100

    smdk4412: FPU corruption fix for Exynos4
    ARM: Exynos: Call cpu_pm_enter/exit in Low Power States.
    
    In low power cpuidle states, the FPU registers get corrupted. By calling
    cpu_pm_enter, the VFP module gets notified so that it saves the FPU state and
    restores it from RAM the next time a thread uses it.
    
    See http://forum.xda-developers.com/showthread.php?p=57637134.
    
    Change-Id: I22cde8180deb68af8c53836e4d08869fea87251b

--- arch/arm/mach-exynos/cpuidle-exynos4.c
+++ arch/arm/mach-exynos/cpuidle-exynos4.c
@@ -10,6 +10,7 @@
 
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/cpu_pm.h>
 #include <linux/cpuidle.h>
 #include <linux/io.h>
 #include <linux/suspend.h>
@@ -584,6 +585,8 @@ static int exynos4_enter_core0_aftr(struct cpuidle_device *dev,
 
 	local_irq_disable();
 
+	cpu_pm_enter();
+
 	if (log_en)
 		pr_info("+++aftr\n");
 
@@ -646,6 +649,8 @@ early_wakeup:
 	if (log_en)
 		pr_info("---aftr\n");
 
+	cpu_pm_exit();
+
 	local_irq_enable();
 	idle_time = (after.tv_sec - before.tv_sec) * USEC_PER_SEC +
 		    (after.tv_usec - before.tv_usec);
@@ -678,6 +683,8 @@ static int exynos4_enter_core0_lpa(struct cpuidle_device *dev,
 	bt_uart_rts_ctrl(1);
 #endif
 	local_irq_disable();
+
+	cpu_pm_enter();
 
 #ifdef CONFIG_INTERNAL_MODEM_IF
 	gpio_set_value(GPIO_PDA_ACTIVE, 0);
@@ -778,6 +785,8 @@ early_wakeup:
 #ifdef CONFIG_INTERNAL_MODEM_IF
 	gpio_set_value(GPIO_PDA_ACTIVE, 1);
 #endif
+
+	cpu_pm_exit();
 
 	local_irq_enable();
 	idle_time = (after.tv_sec - before.tv_sec) * USEC_PER_SEC +
