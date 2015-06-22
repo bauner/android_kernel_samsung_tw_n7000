59085bf0f1ec79ba5e753519541b4141e474cc6b
bauner <bauner@gmx.de>
2015-06-22 22:04:59 +0200

    mmc: replace secdiscard call with panic for extra safety
    A part of this commit from raw kernel:
    https://github.com/libcg/raw_kernel_tw/commit/42c3e05f660a211e4f1e1ce1c02f477c571ae77a

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -2074,7 +2074,7 @@ static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
 			mmc_blk_issue_rw_rq(mq, NULL);
 		if (req->cmd_flags & REQ_SECURE &&
 			!(card->quirks & MMC_QUIRK_MOVINAND_SECURE))
-			ret = mmc_blk_issue_secdiscard_rq(mq, req);
+			panic("secdiscard"); //ret = mmc_blk_issue_secdiscard_rq(mq, req);
 		else
 			ret = mmc_blk_issue_discard_rq(mq, req);
 	} else if (req && req->cmd_flags & REQ_FLUSH) {
